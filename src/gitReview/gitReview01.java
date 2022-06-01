c492268b16d78ff436077a2aa1bdeaccf0c533b1
priyank1194 <priyank9979561561@gmail.com>
2022-05-31 21:51:53 -0400

    2nd commit

--- src/gitReview/gitReview01.java
+++ src/gitReview/gitReview01.java
@@ -4,7 +4,7 @@ public class gitReview01 {
 
 	public static void main(String[] args) {
 		System.out.println("Today is a very hot day");
-
+		System.out.println("Today is a very warm day");
 	}
 
 }
